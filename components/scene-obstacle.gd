eba6f45d39678bc1e74dc40e5e8d1a26b2b563a3
Milan Kormout <m.kormout@gmail.com>
2024-03-09 19:50:31 +0100

    first changes

---
+++ components/scene-obstacle.gd
@@ -0,0 +1,4 @@
+class_name SceneObstacle extends Resource
+
+@export var model: PackedScene
+@export_range(0.0,100.0) var weight: float
---
+++ components/sector.gd
@@ -0,0 +1,5 @@
+class_name Sector extends PackedScene
+
+@export var title: String
+@export var obstacles: Array[SceneObstacle] = []
+@export var color: Color
